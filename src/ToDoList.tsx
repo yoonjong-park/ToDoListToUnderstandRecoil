fb4032afc76f3dfdac7b35f7ceb5ab2f46693810
yoonjong.park <yoonjongpark@gmail.com>
2023-01-13 09:07:39 +0900

    ADD get(categoryState) to return filtered Todos

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import { toDoState, toDoSelector } from "./atoms";
+import React, { FormEvent } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { toDoState, toDoSelector, categoryState } from "./atoms";
 import CreateToDo from "./components/CreateToDo";
 import Todo from "./components/Todo";
 
 const ToDoList = () => {
-  const toDos = useRecoilValue(toDoState);
-  const [toDo, doing, done] = useRecoilValue(toDoSelector); // 구조 분해 할당 https://bit.ly/3k8Q36a
+  const toDos = useRecoilValue(toDoSelector); // 구조 분해 할당 https://bit.ly/3k8Q36a
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (e: FormEvent<HTMLSelectElement>) => {
+    console.log("e", e.currentTarget.value);
+    setCategory(e.currentTarget.value);
+    // target & currentTarget 차이
+    // target = 이벤트 실행 주체
+    // currentTarget = evnet 실행 대상
+  };
 
   return (
     <div>
@@ -14,7 +21,18 @@ const ToDoList = () => {
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
-      <h2>To Do</h2>
+
+      <select onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
+
+      {toDos &&
+        toDos.map(toDo => {
+          return <Todo key={toDo.id} {...toDo} />;
+        })}
+      {/* <h2>To Do</h2>
       {toDo.map((toDo, i) => {
         return <Todo key={toDo.id} {...toDo} />;
       })}
@@ -29,7 +47,7 @@ const ToDoList = () => {
       {done.map((toDo, i) => {
         return <Todo key={toDo.id} {...toDo} />;
       })}
-      <hr />
+      <hr /> */}
     </div>
   );
 };
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -6,6 +6,11 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
@@ -15,10 +20,12 @@ export const toDoSelector = selector({
   key: "todoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter(toDo => toDo.category === "TO_DO"),
-      toDos.filter(toDo => toDo.category === "DOING"),
-      toDos.filter(toDo => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    if (category === "TO_DO")
+      return toDos.filter(toDo => toDo.category === "TO_DO");
+    if (category === "DOING")
+      return toDos.filter(toDo => toDo.category === "DOING");
+    if (category === "DONE")
+      return toDos.filter(toDo => toDo.category === "DONE");
   },
 });
