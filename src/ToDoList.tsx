052204bd34f94ae3ff904d921ed481b2db142b34
yoonjong.park <yoonjongpark@gmail.com>
2023-01-18 08:36:37 +0900

    DEL TRASH command

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -17,7 +17,6 @@ const ToDoList = () => {
 
   return (
     <div>
-      {/* // 모든 Validation을 마친 호출한 이후에, onValid를 실행하게 됨 */}
       <h1>To Dos</h1>
       <hr />
       <select value={category} onInput={onInput}>
